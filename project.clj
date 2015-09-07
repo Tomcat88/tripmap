362c973ba11046ed01c0a80ef1f64e577b50bab4
Thomas <introt@gmail.com>
2015-09-07 11:16:56 +0200

    add http call and polyline extraction

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [clj-http "2.0.0"]]
   :main ^:skip-aot tripmap.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/tripmap/core.clj
+++ src/tripmap/core.clj
@@ -1,6 +1,7 @@
 (ns tripmap.core
   (:gen-class)
-  (:require [clojure.data.json :as json]))
+  (:require [clojure.data.json :as json])
+  (:require [clj-http.client :as http]))
 
 (def directions-endpoint "https://maps.googleapis.com/maps/api/directions/json?")
 
@@ -11,14 +12,23 @@
     (json/read-str contents :key-fn keyword)))
 
 (defn get-direction-query 
-  [{start :start end :end waypoints :waypoints}]
+  [{start :from end :to waypoints :waypoints}]
   (str directions-endpoint "origin=" start "&destination=" end "&waypoints=" (clojure.string/join "|" waypoints)))
 
+(defn get-json
+  [url]
+  (json/read-str (:body (http/get url)) :key-fn keyword))
+
+(defn get-overview-polyline
+  [response-map]
+  ((comp :points :overview_polyline first :routes) response-map))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (println (load-trip "/home/thomas/Documenti/trips/trip1.json")))
+  (let [trip (load-trip "/home/thomas/Documenti/trips/trip1.json")]
+    (println trip)
+    trip))
 
 
 
