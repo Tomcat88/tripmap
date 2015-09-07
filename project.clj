663c65e1097086b881e6674c95d7a1cc5d551153
Thomas <introt@gmail.com>
2015-09-07 15:56:44 +0200

    add utility functions

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http "2.0.0"]]
+                 [clj-http "2.0.0"]
+                 [ring/ring-codec "1.0.0"]]
   :main ^:skip-aot tripmap.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/tripmap/core.clj
+++ src/tripmap/core.clj
@@ -1,9 +1,11 @@
 (ns tripmap.core
   (:gen-class)
   (:require [clojure.data.json :as json])
-  (:require [clj-http.client :as http]))
+  (:require [clj-http.client :as http])
+  (:require [ring.util.codec :as codec]))
 
 (def directions-endpoint "https://maps.googleapis.com/maps/api/directions/json?")
+(def map-endpoint "https://maps.googleapis.com/maps/api/staticmap?size=800x800&")
 
 (defn load-trip
   "Loads a trip JSON into a record from a file"
@@ -15,9 +17,28 @@
   [{start :from end :to waypoints :waypoints}]
   (str directions-endpoint "origin=" start "&destination=" end "&waypoints=" (clojure.string/join "|" waypoints)))
 
+(defn get-map-query
+  [{points :points}]
+  (str map-endpoint "path=enc:" (codec/percent-encode points)))
+
+(defn get-direction-query-list
+  [{options :options trip :trip}]
+  (reduce-kv (fn [urls day locations]
+               (merge-with concat
+                        urls
+                        {day (map get-direction-query locations)})) {} (:days trip)))
+
+
+
 (defn get-json
   [url]
   (json/read-str (:body (http/get url)) :key-fn keyword))
+
+(defn get-map
+  [url]
+  (let [{status :status body :body} (http/get url)]
+    (if (= status 200)
+      body)))
 
 (defn get-overview-polyline
   [response-map]
