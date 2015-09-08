bb36e1ce51caeeabce4db99ce5590e255f58be97
Thomas <introt@gmail.com>
2015-09-08 18:21:24 +0200

    changed trip json format

--- src/tripmap/core.clj
+++ src/tripmap/core.clj
@@ -5,7 +5,17 @@
   (:require [ring.util.codec :as codec]))
 
 (def directions-endpoint "https://maps.googleapis.com/maps/api/directions/json?")
-(def map-endpoint "https://maps.googleapis.com/maps/api/staticmap?size=800x800&")
+(def map-endpoint "https://maps.googleapis.com/maps/api/staticmap?scale=2&")
+
+(defn get-json
+  [url]
+  (json/read-str (:body (http/get url)) :key-fn keyword))
+
+(defn get-map
+  [url]
+  (let [{status :status body :body headers :headers} (http/get url {:as :stream})]
+    (if (= status 200)
+      body)))
 
 (defn load-trip
   "Loads a trip JSON into a record from a file"
@@ -21,46 +31,46 @@
        "&waypoints=" (clojure.string/join "|" waypoints)))
 
 (defn get-map-query
-  [{points :points}]
-  (str "path=enc:" (codec/percent-encode points)))
+  [width height points]
+  (str map-endpoint "size=" width "x" height "&path=enc:" (codec/url-encode points)))
 
-(defn get-direction-query-map
-  [{{width :width height :height :or {width 800 height 800}} :options 
-    {start :start end :end days :days} :trip}]
-  (reduce-kv (fn [urls day locations]
-               (merge-with concat
-                           urls
-                           {day (mapv get-direction-query locations)})) {} days))
-
-(defn get-overview-polyline-map
-  [day-urls-map]
-  (reduce-kv (fn [points day urls]
-               (assoc points day
-                      (mapv (comp get-overview-polyline get-json) urls))) 
-             {} day-urls-map))
-
-
-
-(defn get-json
-  [url]
-  (json/read-str (:body (http/get url)) :key-fn keyword))
-
-(defn get-map
-  [url]
-  (let [{status :status body :body} (http/get url)]
-    (if (= status 200)
-      body)))
 
 (defn get-overview-polyline
-  [response-map]
-  ((comp :points :overview_polyline first :routes) response-map))
+  [url]
+  (-> url
+      get-json
+      :routes
+      first
+      :overview_polyline
+      :points))
+
+(defn json->map
+  [width height]
+  (comp (partial get-map-query width height)
+                       get-overview-polyline 
+                       get-direction-query))
+
+(defn save-img
+  [img filename]
+  (clojure.java.io/copy img (java.io.File. filename)))
 
 (defn -main
   [& args]
-  (println "Hello, World!")
-  (let [trip (load-trip "/home/thomas/Documenti/trips/trip1.json")]
-    (println trip)
-    trip))
+  (let [{
+         {width :width height :height output :output} :options
+         trip :trip} (load-trip "/home/thomas/Documenti/trips/trip1.json")
+         map-url ((json->map width height) trip)
+         img (get-map map-url)
+        ]
+    ;;(println ((json->map width height) trip))
+    (save-img img output)
+    ))
+
+
+
+
+
+
 
 
 
